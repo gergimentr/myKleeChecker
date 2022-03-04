6d9085dca1497723de71625b1880e883217f6912
Georgi Mentr <gergimentr@gmail.com>
2022-03-04 16:56:34 +0300

    Update myworker.py

--- code/myworker.py
+++ code/myworker.py
@@ -31,6 +31,7 @@ loginName = sys.argv[3].split('/')[0]
 passName = sys.argv[2]
 repoName = sys.argv[3].split('/')[1]
 folderName = '/tmp/wklee/'
+tmpOutBuild = '/tmp/outFile.txt'
 
 branchName = fileForWork.split('/')[-1].split('.c')[0]
 commitText = "start work commit"
@@ -39,10 +40,11 @@ os.system('rm -rf '+folderName+'*')
 os.system('cp '+fileForWork+' '+folderName)
 os.system('chmod 777 '+folderName)
 os.system('chmod 777 '+folderName+fileForWork.split('/')[-1])
-os.system('docker run -t -v '+folderName+':/tmp/code --ulimit=\'stack=-1:-1\' klee/klee:2.1 /usr/bin/clang-6.0 -I /home/klee/klee_src/include -emit-llvm -c -g /tmp/code/'+fileForWork.split('/')[-1]+' -o /tmp/code/'+fileForWork.split('/')[-1])
+os.system('docker run -t -v '+folderName+':/tmp/code --ulimit=\'stack=-1:-1\' klee/klee:2.1 /usr/bin/clang-6.0 -I /home/klee/klee_src/include -emit-llvm -c -g /tmp/code/'+fileForWork.split('/')[-1]+' -o /tmp/code/'+fileForWork.split('/')[-1]+' 2>&1 > '+tmpOutBuild)
 if os.path.exists(folderName+fileForWork.split('/')[-1]):
     os.system('timeout --signal=SIGKILL 18000 docker run -t -v /tmp/wklee/:/tmp/code --ulimit=\'stack=-1:-1\' klee/klee:2.1 /home/klee/klee_build/bin/klee --posix-runtime -libc=uclibc /tmp/code/'+fileForWork.split('/')[-1])
-
+else:
+    os.system('cp '+tmpOutBuild+' '+folderName)
 
 save_repo_branch_commit(loginName,passName,repoName,folderName,branchName,"end work commit")
 os._exit(0)
